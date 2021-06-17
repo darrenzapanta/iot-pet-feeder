0c61ab3a6c788e04c2e32c53b2b1c5f83c843008
darrenzapanta <darrenzapanta1@yahoo.com>
2021-06-17 23:31:48 +0800

    add get all route

--- routes/iotFeeder.js
+++ routes/iotFeeder.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const data = await Data.find().sort("date");
+  res.send(data);
+});
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
